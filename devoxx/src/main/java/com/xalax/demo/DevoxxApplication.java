3cf6e4eefb70567136fb932bf5cfc182dc688be8
abdoulay.niang <abdoulay.niang@atos.net>
2020-08-17 01:32:16 +0000

    First commit

---
+++ devoxx/src/main/java/com/xalax/demo/DevoxxApplication.java
@@ -0,0 +1,13 @@
+package com.xalax.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoxxApplication.class, args);
+	}
+
+}
---
+++ devoxx/src/test/java/com/xalax/demo/DevoxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xalax.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
